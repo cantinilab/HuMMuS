26a2fb992c0676e88e1ffa56d6146d9899b2f9f3
r-trimbour <77151348+r-trimbour@users.noreply.github.com>
2026-05-07 21:59:16 +0200

    Remove build artifacts and update .gitignore
    Removes hummuspy/build/, egg-info, __pycache__, .pyc files, container logs,
    and workflow/.anaconda from tracking, and adds them to .gitignore.

--- hummuspy/build/lib/hummuspy/__init__.py
+++
@@ -1,3 +0,0 @@
-import hummuspy.config
-import hummuspy.explore_network
-import hummuspy.core_grn
--- hummuspy/build/lib/hummuspy/utils/get_external_dbs.py
+++
@@ -1,8 +0,0 @@
-import omnipath as om
-
-def get_post_translational_data():
-    ptm = om.interactions.PostTranslational()
-    df = ptm.get()
-    
-    return df
-
